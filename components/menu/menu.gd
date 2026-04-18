3b2d43726956d11cb91d9fe1ac490f20f266c97a
AndrewKarelskiy <andrewkarelskiy@gmail.com>
2026-04-18 16:20:54 +0600

    add menu

---
+++ components/menu/menu.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@onready var _settings_dialog: AcceptDialog = $ui/settings_dialog
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://components/level.tscn")
+
+
+func _on_settings_pressed() -> void:
+	_settings_dialog.popup_centered()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
