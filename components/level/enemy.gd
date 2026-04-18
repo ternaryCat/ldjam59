d3f62830d85e744bc2947e5c64195eb909d50f22
AndrewKarelskiy <andrewkarelskiy@gmail.com>
2026-04-18 13:51:54 +0600

    add enemies texture + better moving

--- components/level/enemy.gd
+++ components/level/enemy.gd
@@ -1,11 +1,32 @@
 extends AnimatedSprite2D
 
+@export var speed: float = 260.0
 
-# Called when the node enters the scene tree for the first time.
+var _in_field: bool = false
+
+@onready var _collision: Area2D = $collision
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	_collision.area_entered.connect(_on_area_entered)
+	_collision.area_exited.connect(_on_area_exited)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	if not _in_field:
+		return
+	if not Input.is_action_pressed("activate"):
+		return
+	var direction := Input.get_vector("left", "right", "top", "down")
+	if direction == Vector2.ZERO:
+		return
+	position += direction * speed * delta
+	rotation = direction.angle()
+
+
+func _on_area_entered(_other: Area2D) -> void:
+	_in_field = true
+
+
+func _on_area_exited(_other: Area2D) -> void:
+	_in_field = false
