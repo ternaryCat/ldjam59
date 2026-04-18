cd2b6ebd176a3b5326b7427f6ce4f892679b651e
AndrewKarelskiy <andrewkarelskiy@gmail.com>
2026-04-18 12:59:54 +0600

    init

---
+++ components/level/player.gd
@@ -0,0 +1,8 @@
+extends Sprite2D
+
+@export var speed: float = 320.0
+
+
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_vector("left", "right", "top", "down")
+	position += direction * speed * delta
